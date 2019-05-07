3f43692bee2d47875e26fc738ada0c86f0c44eb7
JayceLai <goodlaijiaxin@qq.com>
2019-05-07 10:29:28 +0800

    Add port.js that port asmjs to commonjs, change package main prop

---
+++ builds/node/port.js
@@ -0,0 +1,6 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const header = readFileSync('./ammo.js.start').toString();
+const footer = readFileSync('./ammo.js.end').toString();
+const body = readFileSync('./../ammo.js');
+writeFileSync('./ammo.js', [header, body, footer].join('\n'));
