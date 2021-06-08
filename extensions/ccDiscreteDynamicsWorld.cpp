0a3db8a66ffda36f10b76fbcc307fcf9f16d308f
jiaxin.lai <goodlaijiaxin@qq.com>
2021-06-08 18:51:17 +0800

    zero force or torque if damping = 1

--- extensions/ccDiscreteDynamicsWorld.cpp
+++ extensions/ccDiscreteDynamicsWorld.cpp
@@ -1,28 +1,30 @@
 
 #include "ccDiscreteDynamicsWorld.h"
 #include "LinearMath/btScalar.h"
-#include "LinearMath/btQuickprof.h"
-#include "LinearMath/btIDebugDraw.h"
 #include "LinearMath/btVector3.h"
 
-ccDiscreteDynamicsWorld::ccDiscreteDynamicsWorld(btDispatcher *dispatcher,
-                        btBroadphaseInterface *pairCache,
-                        btConstraintSolver *constraintSolver,
-                        btCollisionConfiguration *collisionConfiguration)
-    : btDiscreteDynamicsWorld(dispatcher, pairCache, constraintSolver,collisionConfiguration) {}
+ccDiscreteDynamicsWorld::ccDiscreteDynamicsWorld(
+	btDispatcher *dispatcher, btBroadphaseInterface *pairCache,
+	btConstraintSolver *constraintSolver,
+	btCollisionConfiguration *collisionConfiguration)
+	: btDiscreteDynamicsWorld(dispatcher, pairCache, constraintSolver,
+		collisionConfiguration) {}
 
-void  ccDiscreteDynamicsWorld::applyGravity()
-{
+void ccDiscreteDynamicsWorld::applyGravity() {
 	///@todo: iterate over awake simulation islands!
-	for (int i = 0; i < m_nonStaticRigidBodies.size(); i++)
-	{
-		btRigidBody* body = m_nonStaticRigidBodies[i];
+	for (int i = 0; i < m_nonStaticRigidBodies.size(); i++) {
+		btRigidBody *body = m_nonStaticRigidBodies[i];
 		// compute unconstraint velocity for dynamics
 		if (body->isActive() && !body->isKinematicObject()) {
-			btVector3 linVel = body->getLinearVelocity();
-			const btVector3& gravityAcce = body->getGravity();
-			linVel += btPow(btScalar(1)-body->getLinearDamping(), m_fixedTimeStep) * m_fixedTimeStep * gravityAcce;
-			body->setLinearVelocity(linVel);
+			if (body->getLinearDamping() == 1.) {
+				(const_cast<btVector3 &>(body->getTotalForce())).setZero();
+			}
+			else {
+				body->applyGravity();
+			}
+			if (body->getAngularDamping() == 1.) {
+				(const_cast<btVector3 &>(body->getTotalTorque())).setZero();
+			}
 		}
 	}
 }
