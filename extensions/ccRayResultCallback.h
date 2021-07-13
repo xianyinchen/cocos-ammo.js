047a7a94963632aaa097f6acb40a22e9d77310b8
jiaxin.lai <goodlaijiaxin@qq.com>
2021-07-13 18:01:54 +0800

    support filter query trigger when raycast

--- extensions/ccRayResultCallback.h
+++ extensions/ccRayResultCallback.h
@@ -11,16 +11,25 @@ typedef btCollisionWorld::LocalRayResult LocalRayResult;
 struct ccClosestRayResultCallback : public ClosestRayResultCallback
 {
 	int m_shapePart;
-	
+	bool m_queryTrigger;
+
 	ccClosestRayResultCallback(const btVector3&	rayFromWorld,const btVector3&	rayToWorld)
-	:ClosestRayResultCallback(rayFromWorld, rayToWorld), m_shapePart(0)
+	:ClosestRayResultCallback(rayFromWorld, rayToWorld), m_shapePart(0), m_queryTrigger(true)
 	{
 	}
 
 	// return true when pairs need collision
 	virtual bool needsCollision(btBroadphaseProxy* proxy0) const
 	{
-		return (proxy0->m_collisionFilterGroup & m_collisionFilterMask) != 0;
+		if ((proxy0->m_collisionFilterGroup & m_collisionFilterMask) != 0) {			
+			if (!m_queryTrigger && proxy0->m_clientObject) {
+				auto co = static_cast<btCollisionObject*>(proxy0->m_clientObject);
+				return co->hasContactResponse();
+			}
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	virtual	btScalar	addSingleResult(LocalRayResult& rayResult,bool normalInWorldSpace)
@@ -28,27 +37,44 @@ struct ccClosestRayResultCallback : public ClosestRayResultCallback
 		m_shapePart = rayResult.m_localShapeInfo?rayResult.m_localShapeInfo->m_shapePart:0;
 		return ClosestRayResultCallback::addSingleResult(rayResult, normalInWorldSpace);
 	}
+
+	void setQueryTrigger(bool v) {
+		m_queryTrigger = v;
+	}
 };
 
 struct ccAllHitsRayResultCallback : public AllHitsRayResultCallback
 {
 	btAlignedObjectArray<int> m_shapeParts;
+	bool m_queryTrigger;
 
 	ccAllHitsRayResultCallback(const btVector3&	rayFromWorld,const btVector3&	rayToWorld)
-	:AllHitsRayResultCallback(rayFromWorld, rayToWorld)
+	:AllHitsRayResultCallback(rayFromWorld, rayToWorld), m_queryTrigger(true)
 	{
 	}
 
 	// return true when pairs need collision
 	virtual bool needsCollision(btBroadphaseProxy* proxy0) const
 	{
-		return (proxy0->m_collisionFilterGroup & m_collisionFilterMask) != 0;
+		if ((proxy0->m_collisionFilterGroup & m_collisionFilterMask) != 0) {			
+			if (!m_queryTrigger && proxy0->m_clientObject) {
+				auto co = static_cast<btCollisionObject*>(proxy0->m_clientObject);
+				return co->hasContactResponse();
+			}
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	virtual	btScalar	addSingleResult(LocalRayResult& rayResult,bool normalInWorldSpace)
 	{
 		m_shapeParts.push_back(rayResult.m_localShapeInfo?rayResult.m_localShapeInfo->m_shapePart:0);
 		return AllHitsRayResultCallback::addSingleResult(rayResult, normalInWorldSpace);
+	}
+	
+	void setQueryTrigger(bool v) {
+		m_queryTrigger = v;
 	}
 };
 
