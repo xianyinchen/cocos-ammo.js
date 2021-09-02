38a0d0357b9ade0086d3a7ec09d47f1cc7c8ca2f
jiaxin.lai <goodlaijiaxin@qq.com>
2021-09-02 13:43:43 +0800

    @cocos/bullet@1.0.0-exp.2

--- bullet-release/port.js
+++ bullet-release/port.js
@@ -1,5 +1,6 @@
 const { readFileSync, writeFileSync } = require('fs');
 
-const footer = "export default instantiate;";
+const footer = "const bullet = instantiate;\
+                export default bullet;";
 const body = readFileSync('./bullet.asm.min.js');
 writeFileSync('./bullet.temp.js', [body, footer].join('\n'));
