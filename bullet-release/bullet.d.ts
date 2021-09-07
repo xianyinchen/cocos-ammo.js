8ca1633d5c98af237bb5516cf94a1c4eba25cd26
jiaxin.lai <goodlaijiaxin@qq.com>
2021-09-07 10:07:35 +0800

    update typing

--- bullet-release/bullet.d.ts
+++ bullet-release/bullet.d.ts
@@ -4,13 +4,20 @@ declare function instantiate(env: any, buffer: ArrayBuffer): Bullet.instance;
 declare namespace Bullet {
     type ptr = number;
     interface instance {
+        _malloc(bytes: number): ptr;
+        _free(p: ptr): void;
+        _read_f32(p: ptr): void;
+        _write_f32(p: ptr, v: number): void;
+
         Vec3_new(x: number, y: number, z: number): ptr;
+        Vec3_del(p: ptr): void;
         Vec3_x(p: ptr): number;
         Vec3_y(p: ptr): number;
         Vec3_z(p: ptr): number;
         Vec3_set(p: ptr, x: number, y: number, z: number): void;
 
         Quat_new(x: number, y: number, z: number, w: number): ptr;
+        Quat_del(p: ptr): void;
         Quat_x(p: ptr): number;
         Quat_y(p: ptr): number;
         Quat_z(p: ptr): number;
@@ -18,18 +25,48 @@ declare namespace Bullet {
         Quat_set(p: ptr, x: number, y: number, z: number, w: number): void;
 
         Transform_new(): ptr;
+        Transform_del(p: ptr): void;
         Transform_setIdentity(p: ptr): void;
         Transform_getOrigin(p: ptr): ptr;
         Transform_setRotation(p: ptr, quate: ptr): void;
-        Transform_getRotationRef(p: ptr, quat: ptr): void;
+        Transform_getRotation(p: ptr, quate: ptr): void;
+
+        MotionState_del(p: ptr): void;
+        MotionState_getWorldTransform(p: ptr, transform: ptr): void;
+        MotionState_setWorldTransform(p: ptr, transform: ptr): void;
+        ccMotionState_new(id: number, initTrans: ptr): ptr;
+
+        int_array_size(p: ptr): number;
+        int_array_at(p: ptr, index: number): number;
+        Vec3_array_at(p: ptr, index: number): ptr;
+
+        // constraints
+
+        TypedConstraint_del(p: ptr): void;
+        TypedConstraint_getFixedBody(): ptr;
+        HingeConstraint_new(ptr0: ptr, ptr1: ptr, ptr2: ptr, ptr3: ptr): ptr;
+        HingeConstraint_setFrames(ptr0: ptr, ptr1: ptr, ptr2: ptr): void;
+        P2PConstraint_new(ptr0: ptr, ptr1: ptr, ptr2: ptr, ptr3: ptr): ptr;
+        P2PConstraint_setPivotA(ptr0: ptr, ptr1: ptr): void;
+        P2PConstraint_setPivotB(ptr0: ptr, ptr1: ptr): void;
 
         // shapes
 
+        CollisionShape_del(p: ptr): void;
+        // CollisionShape_getUserIndex(p: ptr): number;
+        // CollisionShape_setUserIndex(p: ptr, i: number): void;
         CollisionShape_isCompound(p: ptr): boolean;
         CollisionShape_setLocalScaling(p: ptr, scale: ptr): void;
         CollisionShape_calculateLocalInertia(p: ptr, mass: number, localInertia: ptr): void;
+        CollisionShape_getAabb(p: ptr, t: ptr, min: ptr, max: ptr): void;
+        CollisionShape_getLocalBoundingSphere(p: ptr): number;
+        CollisionShape_setMargin(p: ptr, margin: number): void;
+        CollisionShape_setMaterial(p: ptr, mat: ptr): void;
+        CollisionShape_setUserPointer(p: ptr, p0: ptr): void;
 
         EmptyShape_static(): ptr;
+
+        ConvexInternalShape_getImplicitShapeDimensions(p: ptr): ptr;
 
         BoxShape_new(p: ptr): ptr;
         BoxShape_setUnscaledHalfExtents(p: ptr, halfExtents: ptr): void;
@@ -37,20 +74,44 @@ declare namespace Bullet {
         SphereShape_new(radius: number): ptr;
         SphereShape_setUnscaledRadius(p: ptr, radius: number): void;
 
+        CylinderShape_new(halfExtents: ptr): ptr;
+        CylinderShape_updateProp(p: ptr, r: number, g: number, d: number): void;
+
+        CapsuleShape_new(radius: number, height: number): ptr;
+        CapsuleShape_updateProp(p: ptr, r: number, g: number, d: number): void;
+
+        ConeShape_new(radius: number, height: number): ptr;
+        ConeShape_setRadius(p: ptr, v: number): void;
+        ConeShape_setHeight(p: ptr, v: number): void;
+        ConeShape_setConeUpIndex(p: ptr, v: number): void;
+
         StaticPlaneShape_new(normal: ptr, constant: number): ptr;
         StaticPlaneShape_getPlaneNormal(p: ptr): ptr;
         StaticPlaneShape_setPlaneConstant(p: ptr, constant: number): void;
 
-        CompoundShape_new(enableDynamicAabbTree: boolean): ptr;
+        TerrainShape_new(i: number, j: number, p: ptr, hs: number, min: number, max: number): ptr;
+
+        TriangleMesh_new(): ptr;
+        TriangleMesh_del(p: ptr): void;
+        TriangleMesh_addTriangle(p: ptr, v0: ptr, v1: ptr, v2: ptr): void;
+        BvhTriangleMeshShape_new(p: ptr, c: boolean, bvh: boolean): ptr;
+        ConvexTriangleMeshShape_new(p: ptr): ptr;
+
+        SimplexShape_new(): ptr;
+        SimplexShape_addVertex(p: ptr, pt: ptr): void;
+
+        ccCompoundShape_new(): ptr;
         CompoundShape_getNumChildShapes(p: ptr): number;
         CompoundShape_getChildShape(p: ptr, i: number): ptr;
         CompoundShape_addChildShape(p: ptr, local: ptr, shape: ptr): void;
         CompoundShape_removeChildShape(p: ptr, shape: ptr): void;
         CompoundShape_updateChildTransform(p: ptr, i: number, trans: ptr, shouldRecalculateLocalAabb: boolean): void;
+        CompoundShape_setMaterial(p: ptr, i: number, f: number, r: number, rf: number, sf: number): void;
 
         // collision
 
         CollisionObject_new(): number;
+        CollisionObject_del(p: ptr): void;
         CollisionObject_getCollisionShape(p: ptr): ptr;
         CollisionObject_setContactProcessingThreshold(p: ptr, contactProcessingThreshold: number): void;
         CollisionObject_getActivationState(p: ptr): number;
@@ -61,9 +122,6 @@ declare namespace Bullet {
         CollisionObject_isKinematicObject(p: ptr): boolean;
         CollisionObject_isStaticObject(p: ptr): boolean;
         CollisionObject_isStaticOrKinematicObject(p: ptr): boolean;
-        CollisionObject_setRestitution(p: ptr, rest: number): void;
-        CollisionObject_setFriction(p: ptr, frict: number): void;
-        CollisionObject_setRollingFriction(p: ptr, frict: number): void;
         CollisionObject_getWorldTransform(p: ptr): ptr;
         CollisionObject_getCollisionFlags(p: ptr): number;
         CollisionObject_setCollisionFlags(p: ptr, flags: number): void;
@@ -75,12 +133,15 @@ declare namespace Bullet {
         CollisionObject_setUserIndex(p: ptr, index: number): void;
         CollisionObject_getUserPointer(p: ptr): number;
         CollisionObject_setUserPointer(p: ptr, userPointer: number): void;
+        CollisionObject_setMaterial(p: ptr, f: number, r: number, rf: number, sf: number): void;
+        CollisionObject_setIgnoreCollisionCheck(p: ptr, p0: ptr, v: boolean): void;
 
-        RigidBody_new(m: number, ms:number): ptr;
+        RigidBody_new(m: number, ms: number): ptr;
         RigidBody_getFlags(p: ptr): number;
         RigidBody_setFlags(p: ptr, flags: number): void;
         RigidBody_setGravity(p: ptr, g: ptr): number;
         RigidBody_setDamping(p: ptr, lin: number, ang: number): void;
+        RigidBody_setMass(p: ptr, m: number);
         RigidBody_setMassProps(p: ptr, m: number, localInertia: ptr): void;
         RigidBody_setLinearFactor(p: ptr, f: ptr): number;
         RigidBody_setAngularFactor(p: ptr, f: ptr): number;
@@ -100,12 +161,9 @@ declare namespace Bullet {
 
         // dynamic
 
-        ccMotionState_new(transform: ptr): ptr;
-        ccMotionState_getWorldTransform(p: ptr, transform: ptr): void;
-        ccMotionState_setWorldTransform(p: ptr, transform: ptr): void;
-
         DefaultCollisionConfiguration_static(): ptr;
-        CollisionDispatcher_new(p: ptr): ptr;
+        CollisionDispatcher_new(): ptr;
+        CollisionDispatcher_del(p: ptr): void;
         Dispatcher_getNumManifolds(p: ptr): number;
         Dispatcher_getManifoldByIndexInternal(p: ptr, i: number): ptr;
 
@@ -117,18 +175,48 @@ declare namespace Bullet {
         PersistentManifold_getBody1(p: ptr): ptr;
         PersistentManifold_getNumContacts(p: ptr): number;
         PersistentManifold_getContactPoint(p: ptr, i: number): ptr;
+        ManifoldPoint_get_m_localPointA(p: ptr): ptr;
+        ManifoldPoint_get_m_localPointB(p: ptr): ptr;
+        ManifoldPoint_get_m_positionWorldOnA(p: ptr): ptr;
+        ManifoldPoint_get_m_positionWorldOnB(p: ptr): ptr;
+        ManifoldPoint_get_m_normalWorldOnB(p: ptr): ptr;
+        ManifoldPoint_get_m_positionWorldOnB(p: ptr): ptr;
 
         DbvtBroadphase_new(): ptr;
+        DbvtBroadphase_del(p: ptr): void;
         SequentialImpulseConstraintSolver_new(): ptr;
+        SequentialImpulseConstraintSolver_del(p: ptr): void;
 
+        CollisionWorld_del(p: ptr): void;
         CollisionWorld_addCollisionObject(p: ptr, body: ptr, g: number, m: number): void;
         CollisionWorld_removeCollisionObject(p: ptr, body: ptr): void;
+        CollisionWorld_rayTest(p: ptr, p0: ptr, p1: ptr, p2: ptr): void;
 
-        ccDiscreteDynamicsWorld_new(dispatcher: ptr, pairCache: ptr, solver: ptr, config: ptr): ptr;
+        ccDiscreteDynamicsWorld_new(dispatcher: ptr, pairCache: ptr, solver: ptr): ptr;
+        ccDiscreteDynamicsWorld_setAllowSleep(p: ptr, v: boolean): void;
         DynamicsWorld_setGravity(p: ptr, g: ptr): void;
         DynamicsWorld_stepSimulation(p: ptr, timeStep: number, maxSubSteps: number, fixedTimeStep: number): ptr;
         DynamicsWorld_addRigidBody(p: ptr, body: ptr, g: number, m: number): void;
         DynamicsWorld_removeRigidBody(p: ptr, body: ptr): void;
+        DynamicsWorld_addConstraint(p: ptr, p2: ptr, v: boolean): void;
+        DynamicsWorld_removeConstraint(p: ptr, p2: ptr): void;
+
+        RayCallback_hasHit(p: ptr): boolean;
+
+        ccAllRayCallback_static(): ptr;
+        ccAllRayCallback_reset(p: ptr, p0: ptr, p1: ptr, m: number, q: boolean): void;
+        ccAllRayCallback_getHitPointWorld(p: ptr): ptr;
+        ccAllRayCallback_getHitNormalWorld(p: ptr): ptr;
+        ccAllRayCallback_getCollisionShapePtrs(p: ptr): ptr;
+
+        ccClosestRayCallback_static(): ptr;
+        ccClosestRayCallback_reset(p: ptr, p0: ptr, p1: ptr, m: number, q: boolean): void;
+        ccClosestRayCallback_getHitPointWorld(p: ptr): ptr;
+        ccClosestRayCallback_getHitNormalWorld(p: ptr): ptr;
+        ccClosestRayCallback_getCollisionShapePtr(p: ptr): ptr;
+
+        ccMaterial_new(): ptr;
+        ccMaterial_set(p: ptr, r: number, f: number, rf: number, sf: number): void;
     }
 }
 
